56864a6c150671c82d6ab4964ab2cd0f81806cf3
Parmeisan <Parmeisan@users.noreply.github.com>
2020-02-01 20:56:04 -0600

    Updated map
    Less repeatable tiles.  Much nicer!

--- godot/src/map/grid/GameBoard.gd
+++ godot/src/map/grid/GameBoard.gd
@@ -63,4 +63,5 @@ func calculate_world_pos(grid_pos : Vector2) -> Vector2:
 func update_tile(x, y, newt):
 	var t_id : Vector2 = Vector2(-6, 5)
 	var new_tile = pawns.get_children()[0].type
+	print("Tile type = %s" % get_cellv(t_id))
 	set_cellv(t_id, new_tile)
