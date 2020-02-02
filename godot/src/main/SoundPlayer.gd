4d0b6c5859076322177299247683e6c7744d57ab
tarastmichel <35859489+tarastmichel@users.noreply.github.com>
2020-02-01 18:42:14 -0600

    started sfx library (added extra sound node and coded in the mc's dialog sfx)

---
+++ godot/src/main/SoundPlayer.gd
@@ -0,0 +1,26 @@
+extends AudioStreamPlayer
+
+# MC Dialog Sounds
+const MCspeech = preload("res://assets/audio/sound/dialog/mc_speech.ogg")
+const MCques = preload("res://assets/audio/sound/dialog/mc_ques.ogg")
+const MCsad = preload("res://assets/audio/sound/dialog/mc_sad.ogg")
+const MCexclaim = preload("res://assets/audio/sound/dialog/mc_exclaim.ogg")
+
+func diaMCspeech():
+	stream = MCspeech
+	play()
+
+func diaMCques():
+	stream = MCques
+	play()
+
+func diaMCsad():
+	stream = MCsad
+	play()
+
+func diaMCexclaim():
+	stream = MCexclaim
+	play()
+
+
+# NPC Character Dialog
