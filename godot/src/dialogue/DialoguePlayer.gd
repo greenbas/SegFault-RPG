efad48ad56399e327552bf15488655932443c573
Steven Greenbank <stevengreenbank9@gmail.com>
2020-02-01 18:37:52 -0600

    Hacked functions into dialogueplayer

--- godot/src/dialogue/DialoguePlayer.gd
+++ godot/src/dialogue/DialoguePlayer.gd
@@ -12,14 +12,15 @@ var expression : String
 var _conversation : Array
 var _index_current : int = 0
 
-onready var Gvars = get_node("/root/GlobalVars")
+onready var gVarsNode = get_node("/root/GlobalVars")
+onready var Gvars = gVarsNode.GlobalVars
 
 func start(dialogue_dict):
 	"""
 	Takes a dictionary of conversation data returned by Dialogue.load()
 	and stores it in an array
 	"""
-	print(Gvars.GlobalVars['lorem'])
+	print(Gvars['lorem'])
 	emit_signal("started")
 	_conversation = dialogue_dict.values()
 	_index_current = 0
@@ -27,6 +28,26 @@ func start(dialogue_dict):
 
 func next():
 	_index_current += 1
+	if _conversation[_index_current].has("type"):
+		var type = _conversation[_index_current].type
+		var arg_1 = _conversation[_index_current].name
+		var arg_2 = _conversation[_index_current].expression
+		var arg_3 = _conversation[_index_current].text
+		if type == 'jump':
+			_index_current = int(arg_1) - 1
+		if type == 'itemcheck':
+			var item = arg_1
+			var then = arg_2
+			var el = arg_3
+			if Gvars[item] == true: 
+				_index_current = int(then) - 1
+			else:
+				_index_current = int(el) - 1
+		if type == 'raise':
+			var item = arg_1
+			Gvars[item] = true
+			_index_current
+			
 	assert(_index_current <= _conversation.size())
 	_update()
 
